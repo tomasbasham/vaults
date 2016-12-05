5559ca8c20680be1a0c531c23696003b0ddd2c33
Tomas Basham <tomasbasham@gmail.com>
2016-12-05 19:25:04 +0000

    Initial commit

---
+++ Vaults/Vaults.h
@@ -0,0 +1,17 @@
+//
+//  Vaults.h
+//  Vaults
+//
+//  Created by Tomas Basham on 05/12/2016.
+//  Copyright © 2016 tomasbasham.co.uk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vaults.
+FOUNDATION_EXPORT double VaultsVersionNumber;
+
+//! Project version string for Vaults.
+FOUNDATION_EXPORT const unsigned char VaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vaults/PublicHeader.h>
---
+++ VaultsTests/VaultsTests.swift
@@ -0,0 +1,35 @@
+//
+//  VaultsTests.swift
+//  VaultsTests
+//
+//  Created by Tomas Basham on 05/12/2016.
+//  Copyright © 2016 tomasbasham.co.uk. All rights reserved.
+//
+
+import XCTest
+@testable import Vaults
+
+class VaultsTests: XCTestCase {
+
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
+    }
+  }
+}
